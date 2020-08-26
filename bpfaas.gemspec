a1274c362fd8837f4a8e8cf83b34b8982a781e04
brainpoint <bpoint.lee@gmail.com>
2020-08-26 17:13:24 +0800

    init

---
+++ bpfaas.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'bpfaas'
+  s.version     = '0.0.1'
+  s.date        = '2020-08-22'
+  s.summary     = 'bpfaas ruby SDK'
+  s.description = 'bpfaas ruby SDK'
+  s.authors     = 'brian'
+  s.email       = 'bpoint.lee@gmail.com'
+  s.files       = ["lib/bpfaas.rb"]
+  s.homepage    = 'http://rubygems.org/gems/bpfaas'
+end
---
+++ lib/bpfaas.rb
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+#================================================================
+#   Copyright (C) 2020 BP Ltd. All rights reserved.
+#   Author      : brian
+#   Created date: 2020-08-22 00:42
+#   Description : 
+#================================================================
+
+class bpfaas
+  def self.hello
+    puts "Hello, bpfaas!"
+  end
+end
